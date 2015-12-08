aaff535e576b5bcddfafa6b7a59e5db925ffe686
Romain ACCIARI <romain.acciari@gmail.com>
2015-12-08 12:21:52 +0100

    New minimal profile

--- minimal/manifests/minimal.pp
+++ minimal/manifests/minimal.pp
@@ -1,8 +1,10 @@
 openiosds::sdsagent {'sds-agent-0':
 }
 openiosds::namespace {'OPENIO':
-  ns => 'OPENIO',
+  ns             => 'OPENIO',
   conscience_url => "${ipaddress}:6000",
+  oioproxy_url   => "${ipaddress}:6006",
+  eventagent_url => "tcp://${ipaddress}:6008",
 }
 openiosds::conscience {'conscience-1':
   num            => '1',
@@ -23,4 +25,19 @@ openiosds::meta2 {'meta2-1':
 openiosds::rawx {'rawx-1':
   num => '1',
   ns => 'OPENIO',
+}
+openiosds::account {'account-1':
+  num        => '1',
+  ns         => 'OPENIO',
+  redis_default_install => true,
+  redis_host => '127.0.0.1',
+  redis_port => '6379',
+}
+openiosds::oioeventagent {'oio-event-agent-1':
+  num       => '1',
+  ns        => 'OPENIO',
+}
+openiosds::oioproxy {'oioproxy-1':
+  num       => '1',
+  ns        => 'OPENIO',
 }
