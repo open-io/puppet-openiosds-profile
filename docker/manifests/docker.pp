6f29b390cbe1aa33638fbd75e226c117782db624
Romain Acciari <romain.acciari@gmail.com>
2017-03-07 14:28:37 +0100

    docker: variables fix

--- docker/manifests/docker.pp
+++ docker/manifests/docker.pp
@@ -1,7 +1,7 @@
 # Default ipaddress to use
 $ipaddr = '127.0.0.1'
-# Comma separated list of tenant:user:passwd:privileges
-$default_tempauth_users = 'demo:demo:DEMO_PASS:.admin'
+# Comma separated list of 'tenant:user:passwd:privileges'
+$default_tempauth_users = ['demo:demo:DEMO_PASS:.admin']
 
 # Deploy a single node
 class {'gridinit':
@@ -17,38 +17,38 @@ openiosds::namespace {'OPENIO':
 }
 openiosds::account {'account-0':
   ns         => 'OPENIO',
-  ipaddress  => "${ipaddr}",
-  redis_host => "${ipaddr}",
+  ipaddress  => $ipaddr,
+  redis_host => $ipaddr,
   no_exec    => true,
 }
 openiosds::conscience {'conscience-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::meta0 {'meta0-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::meta1 {'meta1-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::meta2 {'meta2-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::rawx {'rawx-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::rdir {'rdir-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   location  => "${hostname}-other",
   no_exec   => true,
 }
@@ -58,17 +58,17 @@ openiosds::oioblobindexer {'oio-blob-indexer-rawx-0':
 }
 openiosds::oioeventagent {'oio-event-agent-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::oioproxy {'oioproxy-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::redis {'redis-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::conscienceagent {'conscienceagent-0':
@@ -77,12 +77,12 @@ openiosds::conscienceagent {'conscienceagent-0':
 }
 openiosds::beanstalkd {'beanstalkd-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::ecd {'ecd-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
 openiosds::oioswift {'oioswift-0':
@@ -90,12 +90,12 @@ openiosds::oioswift {'oioswift-0':
   ipaddress        => '0.0.0.0',
   sds_proxy_url    => "http://${ipaddr}:6006",
   auth_system      => 'tempauth',
-  tempauth_users   => [$default_tempauth_users],
+  tempauth_users   => $default_tempauth_users,
   memcache_servers => "${ipaddr}:6019",
   no_exec          => true,
 }
 openiosds::memcached {'memcached-0':
   ns        => 'OPENIO',
-  ipaddress => "${ipaddr}",
+  ipaddress => $ipaddr,
   no_exec   => true,
 }
